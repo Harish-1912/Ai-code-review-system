833a7a400ca1b6afd03c843d4bcfde2b3b8f20ea
Harish-1912 <hariharish12370@gmail.com>
2026-04-15 07:39:57 +0530

    Fix robots.txt location

--- app.py
+++ app.py
@@ -1268,3 +1268,9 @@ if __name__ == "__main__":
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
+from flask import send_from_directory
+import os
+
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory(os.path.dirname(__file__), 'robots.txt')
